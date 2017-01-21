57332095e579258d83bb2df9d3fb3c409163683a
auusi9 <ausi_dalmau@hotmail.com>
2017-01-21 11:49:06 +0100

    Frequency channels and amplitude smooth

--- Assets/Scripts/MicrophoneListener.cs
+++ Assets/Scripts/MicrophoneListener.cs
@@ -20,9 +20,9 @@ public class MicrophoneListener : MonoBehaviour
     //but internally it toggles the output sound to the speakers of the audiosource depending
     //on if the microphone listener is on or off
     public bool disableOutputSound = false;
- 
-     //an audio source also attached to the same object as this script is
-     public AudioSource src;
+
+    //an audio source also attached to the same object as this script is
+    public AudioSource src;
 
     //make an audio mixer from the "create" menu, then drag it into the public field on this script.
     //double click the audio mixer and next to the "groups" section, click the "+" icon to add a 
@@ -34,11 +34,20 @@ public class MicrophoneListener : MonoBehaviour
     //and rename it to "Volume"
     public AudioMixer masterMixer;
 
-
+    public float audioProfile;
     float timeSinceRestart = 0;
 
     public float value;
+    float[] _samples = new float[256];
+    float[] _freqBand = new float[8];
+    float[] _bandBuffer = new float[8];
+    float[] _bufferDecrease = new float[8];
+    float[] _freqBandHighest = new float[8];
+    float[] _audioBand = new float[8];
+    public float[] _audioBandBuffer = new float[8];
 
+    public float _Amplitude, _AmplitudeBuffer;
+    float _amplitudeHighest;
 
     void Start()
     {
@@ -88,6 +97,12 @@ public class MicrophoneListener : MonoBehaviour
 
         //value = .5f + Mathf.Sqrt((value) / 256) * 100;
         this.value = value;
+        GetSpectrumAudioSource();
+        MakeFrequencyBand();
+        BandBuffer();
+        CreateAudioBand();
+        GetAmplitude();
+        AudioProfile(audioProfile);
     }
 
 
@@ -128,7 +143,8 @@ public class MicrophoneListener : MonoBehaviour
         {
             volume = 0.0f;
         }
-        else {
+        else
+        {
             volume = -80.0f;
         }
 
@@ -179,4 +195,95 @@ public class MicrophoneListener : MonoBehaviour
         startMicrophoneListener = true;
     }
 
+    void GetSpectrumAudioSource()
+    {
+        src.GetSpectrumData(_samples, 0, FFTWindow.Blackman);
+
+    }
+    //Create 8 bands of frequency in order from bass to acute
+    void MakeFrequencyBand()
+    {
+        int count = 0;
+
+        for (int i = 0; i < 8; i++)
+        {
+            int sampleCount = (int)Mathf.Pow(2, i);
+            float average = 0;
+            if (i == 7)
+                sampleCount += 2;
+            for (int j = 0; j < sampleCount; j++)
+            {
+                average += _samples[i] * (i + 1);
+                count++;
+            }
+            average /= sampleCount;
+            _freqBand[i] = average * 10;
+        }
+    }
+    //Creates a buffer to smoothly decrease the frequency value
+    void BandBuffer()
+    {
+        for (int i = 0; i < 8; ++i)
+        {
+            if (_freqBand[i] > _bandBuffer[i])
+            {
+                _bandBuffer[i] = _freqBand[i];
+                _bufferDecrease[i] = 0.005f;
+            }
+
+            if (_freqBand[i] < _bandBuffer[i])
+            {
+                _bandBuffer[i] -= _bufferDecrease[i];
+                _bufferDecrease[i] *= 1.2f;
+            }
+        }
+    }
+
+    //Creates a value between 0 and 1 for each band
+    void CreateAudioBand()
+    {
+        for (int i = 0; i < 8; ++i)
+        {
+            if (_freqBand[i] > _freqBandHighest[i])
+            {
+                _freqBandHighest[i] = _freqBand[i];
+            }
+
+            _audioBand[i] = (_freqBand[i] / _freqBandHighest[i]);
+            _audioBandBuffer[i] = (_bandBuffer[i] / _freqBandHighest[i]);
+        }
+    }
+    //Gets the amplitude of the sound
+    void GetAmplitude()
+    {
+        float _CurrentAmplitude = 0;
+        float _CurrentAmplitudeBuffer = 0;
+        for (int i = 0; i < 8; ++i)
+        {
+            _CurrentAmplitude += _audioBand[i];
+            _CurrentAmplitudeBuffer += _audioBandBuffer[i];
+        }
+        if (_CurrentAmplitude > _amplitudeHighest)
+        {
+            _amplitudeHighest = _CurrentAmplitude;
+        }
+        _Amplitude = _CurrentAmplitude / _amplitudeHighest;
+        _AmplitudeBuffer = _CurrentAmplitudeBuffer / _amplitudeHighest;
+    }
+    //Set frequency highest value
+    void AudioProfile(float audioProfile)
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            _freqBandHighest[i] = audioProfile;
+
+        }
+    }
 }
+
+/*
+ 
+     
+     44100/512 = 86.13hz per sample
+
+     */
